f148ec1153cf0312ba5d1b08846c5d7beeeeec7f
ruderude <rude1979@gmail.com>
2021-06-24 13:18:43 +0900

    ９章まで

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -24,11 +24,35 @@ class UserController extends Controller
         $user = User::where('name', $name)->first();
 
         $articles = $user->likes->sortByDesc('created_at');
-        dd($articles);
+        // dd($articles);
 
         return view('users.likes', [
             'user' => $user,
             'articles' => $articles,
+        ]);
+    }
+
+    public function followings(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        $followings = $user->followings->sortByDesc('created_at');
+
+        return view('users.followings', [
+            'user' => $user,
+            'followings' => $followings,
+        ]);
+    }
+
+    public function followers(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        $followers = $user->followers->sortByDesc('created_at');
+
+        return view('users.followers', [
+            'user' => $user,
+            'followers' => $followers,
         ]);
     }
 
